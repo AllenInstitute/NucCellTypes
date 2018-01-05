9b394b85ccc42b961cfb6b443ec6a02272ff2d82
Trygve Bakken <trygveb@alleninstitute.org>
2018-01-04 18:48:09 -0800

    Remove old code

--- R/plot_dendrograms.R
+++
@@ -1,6 +0,0 @@
-load("C:/Users/trygveb/Dropbox/AIBS/Transcriptomics/Manuscripts/WholeCell_vs_Nuc/Figures/Figure 4/dendrograms.rdata", verbose = TRUE)
-
-pdf(file = "C:/Users/trygveb/Dropbox/AIBS/Transcriptomics/Manuscripts/WholeCell_vs_Nuc/Figures/Figure 4/dendrograms.pdf", width = 7, height = 4)
-plot(dendI, las = 1, ylim = c(0, 1))
-plot(dendE, las = 1, ylim = c(0, 1))
-dev.off()
